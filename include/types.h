d01e804d311977d6351cbc86ae8ebea7dbadc7be
Betnix CEO <gstar3181@gmail.com>
2026-09-06 17:40:01 -0400

    Add fixed-width integer types and size_t definition

---
+++ include/types.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+// Unsigned fixed-width integer types
+typedef unsigned char      uint8_t;
+typedef unsigned short     uint16_t;
+typedef unsigned int       uint32_t;
+typedef unsigned long long uint64_t;
+
+// Signed fixed-width integer types
+typedef signed char        int8_t;
+typedef signed short       int16_t;
+typedef signed int         int32_t;
+typedef signed long long   int64_t;
+
+// Memory size type
+typedef uint32_t size_t;
+
+#endif
